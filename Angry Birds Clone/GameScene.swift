8e21b1e1df2fec35409ce4d454c47bc2be360574
Abe <abhi.rawat456@gmail.com>
2020-02-29 12:16:35 +0530

    Added code to make the bird move

--- Angry Birds Clone/GameScene.swift
+++ Angry Birds Clone/GameScene.swift
@@ -36,9 +36,9 @@ class GameScene: SKScene {
         
         bird = childNode(withName: "bird") as! SKSpriteNode
         bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture.size().height / 14)
-        bird.physicsBody?.affectedByGravity = true
+        bird.physicsBody?.affectedByGravity = false
         bird.physicsBody?.isDynamic = true
-        bird.physicsBody?.mass = 0.5
+        bird.physicsBody?.mass = 0.3
         
         //Box
         let boxTexture = SKTexture(imageNamed: "brick")
@@ -92,18 +92,17 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 200))
+        bird.physicsBody?.affectedByGravity = true
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
     }
     
     
